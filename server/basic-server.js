ec0eba6347342df836ce78a8f9264d06b0b3e01a
shea-hawkins <shea.m.hawkins@gmail.com>
2016-06-27 21:13:23 -0700

    Serve files using Express.js

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,39 +1,34 @@
 /* Import node's http module: */
-var http = require('http');
-var handler = require('./request-handler.js');
+var express = require('express');
+var path = require('path');
+var messageServer = require('./message-server');
+var app = express();
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
 var port = 3000;
-
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
 var ip = '127.0.0.1';
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handler.requestHandler);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+//app.get('/index', function());
 
+// app.use('/client', express.static('client'));
+//app.use('/index.html', express.send('client/index.html'));
+
+
+//express.static(__dirname + '/client/client'));
+
+
+app.get('/classes/messages', messageServer.get);
+app.post('/classes/messages', messageServer.post);
+app.options('/classes/messages', messageServer.options);
+app.use('/client', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
+app.use('/', express.static(path.join(__dirname, '../client')));
+app.listen(port, function() {
+  console.log('Listening on http://' + ip + ':' + port);
+});
