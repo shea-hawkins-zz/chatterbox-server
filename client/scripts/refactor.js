918451c3ab6cfe8377301d6e4503a85c408a5af7
shea-hawkins <shea.m.hawkins@gmail.com>
2016-06-28 16:28:09 -0700

    Finalize refactor.

--- client/scripts/refactor.js
+++ client/scripts/refactor.js
@@ -22,7 +22,7 @@
 
 app = {
 
-  server: 'https://api.parse.com/1/classes/messages/',
+  server: 'http://127.0.0.1:3000/classes/messages',
 
   init: function() {
     // Get username
@@ -76,7 +76,6 @@ app = {
   loadMsgs: function() {
     $.ajax({
       url: app.server,
-      data: { order: '-createdAt' },
       contentType: 'application/json',
       success: function(json) {
         app.displayMessages(json.results);
@@ -121,7 +120,7 @@ app = {
 /////////////////////////////////////////////////////////////////////////////
 
 var Message = Backbone.Model.extend({
-  url: 'https://api.parse.com/1/classes/messages/',
+  url: 'http://127.0.0.1:3000/classes/messages',
   defaults: {
     username: '',
     text: ''
@@ -131,10 +130,10 @@ var Message = Backbone.Model.extend({
 var Messages = Backbone.Collection.extend({
 
   model: Message,
-  url: 'https://api.parse.com/1/classes/messages/',
+  url: 'http://127.0.0.1:3000/classes/messages',
 
   loadMsgs: function() {
-    this.fetch({data: { order: '-createdAt' }});
+    this.fetch({});
   },
 
   parse: function(response, options) {
